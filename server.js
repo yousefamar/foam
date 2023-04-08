58d71ad0a0207aca64e8f2ab01a3f07524c278d0
Yousef Amar <yousefamar@gmail.com>
2023-04-08 18:31:14 +0000

    add(server): rebuild endpoint, on patch, and caddy compat

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const fs = require('fs');
 const { join } = require('path');
+const { exec } = require('child_process');
 const express = require('express');
 const Eleventy = require('@11ty/eleventy');
 const fm = require('front-matter');
 const app = express();
 const port = process.env.PORT || 8080;
 const rootDir = process.env.ROOT_DIR || '/home/amar/doc/brain/root/';
+
+async function runEleventy() {
+  return new Promise((resolve, reject) => {
+    exec('npx eleventy', (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(stdout);
+      }
+    });
+  });
+}
 
 const writeUpdate = (path, newContent) => {
   newContent = newContent.replace(/</g, '&lt;').replace(/>/g, '&gt;');
@@ -39,13 +52,15 @@ const writeUpdate = (path, newContent) => {
 
 app.use(express.json());
 
-app.patch('/*', (req, res) => {
+app.patch('/*', async (req, res) => {
   let path = req.path;
 
-  if (!path.startsWith('/memo/'))
-    return res.status(400).json({ success: false, error: 'Invalid path' });
+  // New Caddy config only allows this on /memo/ anyway
+  // if (!path.startsWith('/memo/'))
+  //   return res.status(400).json({ success: false, error: 'Invalid path' });
 
-  path = path.replace('/memo/', rootDir);
+  // path = path.replace('/memo/', rootDir);
+  path = path.replace('/', rootDir);
 
   const { content } = req.body;
 
@@ -54,14 +69,43 @@ app.patch('/*', (req, res) => {
 
   try {
     writeUpdate(path, content);
+    console.log(new Date(), 'Eleventy build started');
+    //await new Eleventy().write();
+    try {
+      const output = await runEleventy();
+      console.log(output);
+    } catch (error) {
+      console.error(error);
+    }
+    console.log(new Date(), 'Eleventy build finished');
   } catch (error) {
     return res.status(400).json({ success: false, error: error.message });
   }
   res.json({ success: true });
 });
 
+app.get('/rebuild', async (req, res) => {
+  try {
+    console.log(new Date(), 'Eleventy build started');
+    //await new Eleventy().write();
+    try {
+      const output = await runEleventy();
+      console.log(output);
+    } catch (error) {
+      console.error(error);
+    }
+    console.log(new Date(), 'Eleventy build finished');
+  } catch (error) {
+    return res.status(500).json({ success: false, error: error.message });
+  }
+  res.json({ success: true });
+});
+
 (async function () {
-  await new Promise(resolve => app.listen(port, resolve));
-  console.log(`Server started on port ${port}`);
-  await new Eleventy().watch();
+  try {
+    await new Promise(resolve => app.listen(port, resolve));
+    console.log(`Server started on port ${port}`);
+  } catch (error) {
+    console.error(error);
+  }
 })();
